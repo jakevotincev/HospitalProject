ba295cc23840cc906f8476e66d158582f39292ce
jakev <jakevotincev@gmail.com>
2021-06-13 01:22:56 +0300

    added hospital service

--- frontend/src/app/patient/patient.component.ts
+++ frontend/src/app/patient/patient.component.ts
@@ -5,7 +5,7 @@ import {Observable} from "rxjs";
 import {map, startWith} from "rxjs/operators";
 import {HospitalService} from "../services/hospital.service";
 
-//todo: add service
+
 @Component({
   selector: 'app-patient',
   templateUrl: './patient.component.html',

5f1cef0b64c1e4b92fefe4209d6b390db23be8f6
jakev <jakevotincev@gmail.com>
2021-06-12 22:11:02 +0300

    added hospital search bar with autocomplete

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,15 @@ import {MatSliderModule} from "@angular/material/slider";
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
-import { AppRoutingModule } from './app-routing.module';
-import { PatientComponent } from './patient/patient.component';
-import { DoctorComponent } from './doctor/doctor.component';
+import {HeaderComponent} from './header/header.component';
+import {AppRoutingModule} from './app-routing.module';
+import {PatientComponent} from './patient/patient.component';
+import {DoctorComponent} from './doctor/doctor.component';
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {MatOptionModule} from "@angular/material/core";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -20,7 +25,13 @@ import { DoctorComponent } from './doctor/doctor.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatSliderModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatAutocompleteModule,
+    MatOptionModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/patient/patient.component.ts
+++ frontend/src/app/patient/patient.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Hospital} from "../interfaces/hospital";
+import {FormControl} from "@angular/forms";
+import {Observable} from "rxjs";
+import {map, startWith} from "rxjs/operators";
 
+//todo: add service
 @Component({
   selector: 'app-patient',
   templateUrl: './patient.component.html',
@@ -7,9 +12,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientComponent implements OnInit {
 
-  constructor() { }
+  selectedHospital = new FormControl();
+  filteredOptions?: Observable<Hospital[]>;
 
-  ngOnInit(): void {
+  hospitals: Hospital[] = [
+    {
+      "name": "поликлиника №1",
+      "address": "улица1",
+      "id": 1
+    },
+    {
+      "name": "поликлиника №2",
+      "address": "улица2",
+      "id": 2
+    },
+    {
+      "name": "поликлиника №3",
+      "address": "улица3",
+      "id": 3
+    },
+    {
+      "name": "поликлиника №4",
+      "address": "улица4",
+      "id": 4
+    },
+    {
+      "name": "хуйня",
+      "address": "улица4",
+      "id": 4
+    }
+  ];
+
+  private _filter(value: string): Hospital[] {
+    const filterValue = value.toLowerCase();
+
+    return this.hospitals.filter(hospital => this.displayFn(hospital).toLowerCase().includes(filterValue));
   }
 
+  displayFn(hospital: Hospital): string {
+    return hospital && hospital.name ? hospital.name : '';
+  }
+
+  ngOnInit(): void {
+    this.filteredOptions = this.selectedHospital.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => typeof value === 'string' ? value : value.name),
+        map(name => name ? this._filter(name) : this.hospitals.slice())
+      );
+  }
 }

e283414f43c4644b98d72bc44a8f74f1c98458c0
jakev <jakevotincev@gmail.com>
2021-07-04 01:48:45 +0800

    added mockaroo test data

---
+++ backend/mockaroo/Hospital.sql
@@ -0,0 +1,13 @@
+insert into Hospital (id, address, name) values (1, '5 Sommers Drive', 'Hirthe Group');
+insert into Hospital (id, address, name) values (2, '24260 Pleasure Alley', 'Steuber, Rodriguez and Kautzer');
+insert into Hospital (id, address, name) values (3, '7 Briar Crest Parkway', 'Rolfson-Lakin');
+insert into Hospital (id, address, name) values (4, '66 Maple Wood Plaza', 'Greenholt-Renner');
+insert into Hospital (id, address, name) values (5, '0518 Bonner Circle', 'McGlynn, Douglas and Zemlak');
+insert into Hospital (id, address, name) values (6, '49 Melby Drive', 'Witting, Blick and Schuster');
+insert into Hospital (id, address, name) values (7, '38 Boyd Hill', 'Crist Inc');
+insert into Hospital (id, address, name) values (8, '6675 Arrowood Parkway', 'O''Keefe, Crona and Raynor');
+insert into Hospital (id, address, name) values (9, '01169 Scofield Drive', 'Hane, Abshire and Wilkinson');
+insert into Hospital (id, address, name) values (10, '5 Holy Cross Plaza', 'Halvorson, Larkin and McClure');
+insert into Hospital (id, address, name) values (11, '34 Goodland Avenue', 'Beatty LLC');
+insert into Hospital (id, address, name) values (12, '15 Pearson Lane', 'Berge and Sons');
+insert into Hospital (id, address, name) values (13, '8545 Gale Trail', 'Von Group');

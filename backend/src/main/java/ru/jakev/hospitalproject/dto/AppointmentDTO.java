66931283c9a15a761c20b0cf628bba3b4b36a94b
Evgenii Votintsev <evotintsev@devexperts.com>
2022-01-29 10:03:35 +0300

    add redis cache and security

--- backend/src/main/java/ru/jakev/hospitalproject/dto/AppointmentDTO.java
+++ backend/src/main/java/ru/jakev/hospitalproject/dto/AppointmentDTO.java
@@ -5,14 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class AppointmentDTO {
+public class AppointmentDTO implements Serializable {
 
+    private static final long serialVersionUID = 4074927415985512005L;
     private Integer id;
     private DoctorDTO doctor;
     private PatientDTO patient;
